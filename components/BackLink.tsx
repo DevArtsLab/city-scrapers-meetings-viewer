d5780e1ef89fec2257d9835c24bdb5585d032632
Amir Olyaei <amirholyaei@gmail.com>
2026-07-28 14:38:36 -0400

    Replace custom SVG arrow with MUI ArrowBack icon in BackLink component

--- components/BackLink.tsx
+++ components/BackLink.tsx
@@ -1,23 +1,6 @@
 import MuiLink from "@mui/material/Link";
+import ArrowBack from "@mui/icons-material/ArrowBack";
 import NextLink from "@/components/NextLink";
-
-function ArrowBackIcon() {
-  return (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-    >
-      <path d="M19 12H5M12 19l-7-7 7-7" />
-    </svg>
-  );
-}
 
 interface BackLinkProps {
   /** Destination to navigate back to. */
@@ -43,7 +26,7 @@ export default function BackLink({ href, label }: BackLinkProps) {
         color: "primary.main",
       }}
     >
-      <ArrowBackIcon />
+      <ArrowBack fontSize="small" />
       {label}
     </MuiLink>
   );
