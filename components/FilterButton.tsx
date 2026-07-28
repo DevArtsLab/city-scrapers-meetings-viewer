c8aeea75d4fd74af9277c126092de7f8c41a92cf
Amir Olyaei <amirholyaei@gmail.com>
2026-07-28 14:51:07 -0400

    Replace custom SVG icons with MUI icons in FilterButton and panel components

--- components/FilterButton.tsx
+++ components/FilterButton.tsx
@@ -2,6 +2,7 @@
 
 import type { Ref } from "react";
 import Button from "@mui/material/Button";
+import FilterList from "@mui/icons-material/FilterList";
 
 interface FilterButtonProps {
   /** Whether the panel this button controls is currently open. */
@@ -11,20 +12,6 @@ interface FilterButtonProps {
   panelId: string;
   /** Ref to the underlying button element. */
   ref?: Ref<HTMLButtonElement>;
-}
-
-function FilterIcon() {
-  return (
-    <svg
-      width="18"
-      height="18"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      aria-hidden="true"
-    >
-      <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z" />
-    </svg>
-  );
 }
 
 /** Toggle button for the filters panel. Rendered in MeetingsToolbar. */
@@ -41,7 +28,7 @@ export default function FilterButton({
       size="small"
       disableRipple
       onClick={onToggle}
-      startIcon={<FilterIcon />}
+      startIcon={<FilterList fontSize="small" />}
       aria-expanded={open}
       aria-controls={panelId}
       sx={{
