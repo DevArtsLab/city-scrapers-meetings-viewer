36e8796598719e5ad0ff32e668a333c74053d935
Amir Olyaei <amirholyaei@gmail.com>
2026-08-21 11:59:03 -0400

    Reduce app layout max width from xl to lg for tighter content presentation

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -6,7 +6,7 @@ const PAGE_SPACING = { pt: { xs: 2, sm: 3 }, pb: { xs: 3, sm: 6 } };
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AppShell>
-      <Container maxWidth="xl" sx={PAGE_SPACING}>
+      <Container maxWidth="lg" sx={PAGE_SPACING}>
         {children}
       </Container>
     </AppShell>
